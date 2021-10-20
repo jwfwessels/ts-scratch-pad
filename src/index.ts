0bb9e4776efa621002a3ebba14e3cc22ea840cb3
jwfwessels <jwfwessels@gmail.com>
2021-10-20 16:29:28 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import {setupTypeAcquisition} from "@typescript/ata"
+import * as ts from "typescript"
+
+console.log({setupTypeAcquisition, ts})
+
+// Create the function for running ATA with a series of callbacks
+const ata = setupTypeAcquisition({
+    projectName: "My ATA Project",
+    typescript: ts,
+    logger: console,
+    delegate: {
+      receivedFile: (path: string, code: string) => {
+        // Add code to your runtime at the path...
+      },
+      started: () => {
+        console.log("ATA start")
+      },
+      progress: (downloaded: number, total: number) => {
+        console.log(`Got ${downloaded} out of ${total}`)
+      },
+      finished: vfs => {
+        console.log("ATA done", vfs)
+      },
+    },
+  })
+  
+  // Run that function with the new sourcefile
+  ata(`import danger from "danger"`)
